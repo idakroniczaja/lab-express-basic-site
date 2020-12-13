efdbd3dde7040375f45e7d2f0be9cc4df0df1e72
ida <so.kida@hotmail.com>
2020-12-12 20:05:17 -0500

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get ('/home', (req, res)=>{
+  res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get ('/about', (req, res)=>{
+  res.sendFile(__dirname + "/views/about.html")
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/photoGalery", (req, res) => {
+  res.sendFile(__dirname + "/views/photoGalery.html");
+});
+
+app.listen(3000, ()=>{
+console.log('running')
+})
